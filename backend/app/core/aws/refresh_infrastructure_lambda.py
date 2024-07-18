b9c1f74c09a77c9d45c113a1ba6a3020b935be72
Stijn Janssens <janssens.stijn@hotmail.com>
2024-07-18 15:04:38 +0200

    Fix exception handling for unit tests

--- backend/app/core/aws/refresh_infrastructure_lambda.py
+++ backend/app/core/aws/refresh_infrastructure_lambda.py
@@ -1,6 +1,8 @@
 from logging import getLogger
 from typing import Any
 
+from botocore.exceptions import ClientError
+
 from app.core.aws.boto3_clients import get_client
 from app.settings import settings
 
@@ -14,11 +16,15 @@ class RefreshInfrastructureLambda:
 
     def trigger(self) -> dict[str, Any]:
         if self.enabled:
-            response = self.client.invoke(
-                FunctionName=self.lambda_arn, InvocationType="Event"
-            )
-            self.logger.info(response)
-            return response
+            try:
+                response = self.client.invoke(
+                    FunctionName=self.lambda_arn, InvocationType="Event"
+                )
+                self.logger.info(response)
+                return response
+            except ClientError:
+                self.logger.warning("Triggering failed", exc_info=1)
+                return {"status": "triggering failed"}
         else:
             self.logger.info("No infrastructure lambda is set up, not triggering.")
             return {"status": "not enabled"}
