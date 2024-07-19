18ea50141a25492513a2425179b82834354c717e
Kristof Martens <kristof.martens@dataminded.be>
2024-07-19 13:43:42 +0200

    Update state file configuration

--- integrations/terraform/envs/portal/state.tf
+++ integrations/terraform/envs/portal/state.tf
@@ -1,11 +1,17 @@
 terraform {
   backend "s3" {
-    key = "terraform/cvr-pbac/infrastructure/state.tfstate"
+    # Path where terraform state file will to be stored
+    key = "terraform/portal/infrastructure/state.tfstate"
 
+    # AWS region of the S3 bucket
     region         = "eu-west-1"
-    bucket         = "cvr-pbac-s3-infrastructure-demo-jrivmq"
-    kms_key_id     = "arn:aws:kms:eu-west-1:130966031144:key/41c40b50-cf9c-4afe-8333-37eacdfc9f54"
+    # Name of the bucket where the terraform state will be stored in
+    bucket         = "data-product-portal-infrastructure-kjsdfc/"
+    # KMS key arn for the S3 bucket key to encrypt the terraform state with
+    kms_key_id     = "arn:aws:kms:eu-west-1:130966031144:key/xxxxxxxx"
+    # Whether the terraform state needs to be encrypted, say yes here ;)
     encrypt        = true
-    dynamodb_table = "terraform-state-locks-conveyor-demo"
+    # Dynamo table to store terraform state locks
+    dynamodb_table = "terraform-state-locks-portal"
   }
 }
