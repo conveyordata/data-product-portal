1231d74d7ec70e7818bcb5e0c446136c9cfd5086
Andrii Ivaniuk <andrii.ivaniuk@gmail.com>
2024-07-26 10:45:59 +0300

    Add APIs to manage settings (#50)
    * Add APIs to manage settings
    
    * Removed breakpoint
    
    * APIs to manage settings
    
    * Removed commented code
    
    * Added endpoint to get specific platform
    
    * Refactored environment service
    
    ---------
    
    Co-authored-by: Andrii Ivaniuk <andrii.ivaniuk@sentiocx.com>

--- backend/app/environments/schema.py
+++ backend/app/environments/schema.py
@@ -1,7 +1,46 @@
+import json
+from uuid import UUID
+
+from pydantic import Field, field_validator
+
 from app.shared.schema import ORMModel
+
+from .enums import PlatformTypes
 
 
 class Environment(ORMModel):
     name: str
-    context: str
     is_default: bool = False
+
+
+class S3Config(ORMModel):
+    bucket_arn: str
+    prefix_path: str
+
+
+class GlueConfig(ORMModel):
+    db_schema: str = Field(alias="schema")
+    table_prefixes: list[str]
+
+
+class AWSSettings(ORMModel):
+    account_id: str
+    kms_key: str
+    s3: S3Config
+    glue: GlueConfig
+
+
+class CreatePlatform(ORMModel):
+    name: PlatformTypes
+    settings: AWSSettings
+
+    @field_validator("settings", mode="before")
+    @classmethod
+    def parse_settings(cls, v: str | dict) -> dict:
+        if isinstance(v, str):
+            return json.loads(v)
+        return v
+
+
+class Platform(CreatePlatform):
+    id: UUID
---
+++ backend/tests/factories/user.py
@@ -0,0 +1,16 @@
+import factory
+from factory.fuzzy import FuzzyText
+
+from app.users.model import User
+
+
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = User
+
+    id = factory.Faker("uuid4")
+    email = factory.Faker("email")
+    external_id = FuzzyText(length=10)
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    is_admin = False
