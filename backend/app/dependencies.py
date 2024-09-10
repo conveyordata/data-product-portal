588363cec82bba3af2871a3234aa51a48030e275
Stijn Janssens <janssens.stijn@hotmail.com>
2024-09-10 10:32:26 +0200

    Improve endpoint security with data product role dependency (#84)
    * Introduce new dependency to check for certain membership role type when accessing APIs
    
    * Owners should be allowed the same things as members always
    
    * Update unit tests with new security dependencies
    
    * Renaming and default access scheme, move to sqlalchemy query style 2.0

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,6 +1,15 @@
+from typing import Optional
+from uuid import UUID
+
 from fastapi import Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from app.core.auth.auth import get_authenticated_user
+from app.data_product_memberships.enums import DataProductUserRole
+from app.data_products.model import DataProduct as DataProductModel
+from app.database.database import get_db_session
 from app.users.schema import User
 
 
@@ -10,3 +19,52 @@ async def only_for_admin(authenticated_user: User = Depends(get_authenticated_us
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Only admin can execute this operation",
         )
+
+
+class OnlyWithProductAccess:
+    """Only allow users that have a membership of a certain role type to this product"""
+
+    def __init__(self, allowed_roles: Optional[list[DataProductUserRole]] = None):
+        if not allowed_roles:
+            allowed_roles = [DataProductUserRole.OWNER, DataProductUserRole.MEMBER]
+        self.allowed_roles = allowed_roles
+
+    def __call__(
+        self,
+        data_product_name: Optional[str] = None,
+        id: Optional[UUID] = None,
+        authenticated_user: User = Depends(get_authenticated_user),
+        db: Session = Depends(get_db_session),
+    ) -> None:
+        try:
+            if id:
+                data_product = db.scalars(
+                    select(DataProductModel).filter_by(id=id)
+                ).one()
+
+            elif data_product_name:
+                data_product = db.scalars(
+                    select(DataProductModel).filter_by(external_id=data_product_name)
+                ).one()
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Provide at least a product name or id",
+                )
+        except NoResultFound:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="product not founds"
+            )
+        if (
+            authenticated_user.id
+            not in [
+                membership.user_id
+                for membership in data_product.memberships
+                if membership.role in self.allowed_roles
+            ]
+            and not authenticated_user.is_admin
+        ):
+            raise HTTPException(
+                status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to assume this role",
+            )
