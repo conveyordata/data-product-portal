5af91b9d226904c9af7a7ced53df3cef720bc996
Stijn Janssens <janssens.stijn@hotmail.com>
2024-09-24 12:53:51 +0200

    Remove obsolete tests for create environment

--- backend/tests/environments/test_router.py
+++ backend/tests/environments/test_router.py
@@ -1,28 +1,18 @@
 import json
 
 import pytest
-from sqlalchemy.exc import IntegrityError
 
 from ..factories import (
     EnvironmentFactory,
     EnvPlatformConfigFactory,
     EnvPlatformServiceConfigFactory,
     PlatformServiceFactory,
-    UserFactory,
 )
 
 ENDPOINT = "/api/envs"
 
 
 class TestEnvironmentsRouter:
-    def test_create_environment(self, client):
-        UserFactory(external_id="sub", is_admin=True)
-        response = self.create_environment(
-            client,
-            {"name": "dev", "context": "environment_context{{}}", "is_default": False},
-        )
-        assert response.status_code == 200
-
     @pytest.mark.usefixtures("admin")
     def test_get_environments(self, client):
         env_obj = EnvironmentFactory()
@@ -32,24 +22,6 @@ class TestEnvironmentsRouter:
         environments = response.json()
         assert len(environments) == 1
         assert environments[0]["name"] == env_obj.name
-
-    @pytest.mark.usefixtures("admin")
-    def test_create_environment_with_repeated_name(self, client):
-        env_obj = EnvironmentFactory()
-        with pytest.raises(IntegrityError):
-            self.create_environment(
-                client,
-                {
-                    "name": env_obj.name,
-                    "context": "environment_context{{}}",
-                    "is_default": False,
-                },
-            )
-
-    @staticmethod
-    def create_environment(client, environment):
-        response = client.post(ENDPOINT, json=environment)
-        return response
 
     def test_get_environment_platform_service_config_forbidden(self, client):
         response = client.get(
