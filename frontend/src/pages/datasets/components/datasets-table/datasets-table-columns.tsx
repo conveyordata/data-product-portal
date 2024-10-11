38f30d35151034cc3e9a102ca608b968243ec56a
Mathias Bongaerts <mathiasbongaerts@hotmail.com>
2024-10-11 15:08:13 +0200

    Made datasets table colums filterable

--- frontend/src/pages/datasets/components/datasets-table/datasets-table-columns.tsx
+++ frontend/src/pages/datasets/components/datasets-table/datasets-table-columns.tsx
@@ -2,15 +2,17 @@ import { DatasetAccess, DatasetsGetContract } from '@/types/dataset';
 import { TFunction } from 'i18next';
 import { Popover, TableColumnsType } from 'antd';
 import i18n from '@/i18n.ts';
+import { getStatusLabel } from '@/utils/status.helper.ts';
 import { TableStatusTag } from '@/components/list/table-status-tag/table-status-tag.component.tsx';
 import { TableCellItem } from '@/components/list/table-cell-item/table-cell-item.component.tsx';
 import { BusinessAreaContract } from '@/types/business-area';
 import { CustomSvgIconLoader } from '@/components/icons/custom-svg-icon-loader/custom-svg-icon-loader.component.tsx';
 import shieldHalfIcon from '@/assets/icons/shield-half-icon.svg?react';
 import { getDatasetAccessTypeLabel } from '@/utils/access-type.helper.ts';
+import { FilterSettings } from '@/utils/table-filter.helper';
 
 const iconColumnWidth = 30;
-export const getDatasetTableColumns = ({ t }: { t: TFunction }): TableColumnsType<DatasetsGetContract[0]> => [
+export const getDatasetTableColumns = ({ t, datasets }: { t: TFunction, datasets: DatasetsGetContract }): TableColumnsType<DatasetsGetContract[0]> => [
     {
         title: t('Id'),
         dataIndex: 'id',
@@ -34,15 +36,14 @@ export const getDatasetTableColumns = ({ t }: { t: TFunction }): TableColumnsTyp
     {
         title: t('Name'),
         dataIndex: 'name',
-        filterSearch: true,
         defaultSortOrder: 'ascend',
-        onFilter: (value, record) => record.name.startsWith(value as string),
         ellipsis: {
             showTitle: false,
         },
         render: (name: string) => {
             return <TableCellItem text={name} tooltip={{ content: name }} />;
         },
+        ...new FilterSettings(datasets, ds => ds.name),
     },
     {
         title: t('Status'),
@@ -50,25 +51,25 @@ export const getDatasetTableColumns = ({ t }: { t: TFunction }): TableColumnsTyp
         render: (status) => {
             return <TableStatusTag status={status} />;
         },
+        ...new FilterSettings(datasets, ds => getStatusLabel(ds.status)),
     },
     {
         title: t('Business Area'),
         dataIndex: 'business_area',
-        filterSearch: true,
-        onFilter: (value, record) => record.description.startsWith(value as string),
         render: (businessArea: BusinessAreaContract) => {
             return <TableCellItem text={businessArea.name} />;
         },
         ellipsis: true,
+        ...new FilterSettings(datasets, ds => ds.business_area.name),
     },
     {
         title: t('Access Type'),
         dataIndex: 'access_type',
-        filterSearch: true,
         render: (accessType: DatasetAccess) => {
             return <TableCellItem text={getDatasetAccessTypeLabel(accessType)} />;
         },
         ellipsis: true,
+        ...new FilterSettings(datasets, ds => getDatasetAccessTypeLabel(ds.access_type)),
     },
     {
         title: t('Shared With'),
