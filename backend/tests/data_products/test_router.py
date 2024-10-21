9fbf2bce25a126a65222b60397b97f1554053ae4
Stijn Janssens <janssens.stijn@hotmail.com>
2024-10-21 15:16:36 +0200

    Add extra tests

--- backend/tests/data_products/test_router.py
+++ backend/tests/data_products/test_router.py
@@ -7,6 +7,7 @@ from tests.factories import (
     DataProductTypeFactory,
     UserFactory,
 )
+from tests.factories.data_output import DataOutputFactory
 from tests.factories.data_product_membership import DataProductMembershipFactory
 
 from app.data_product_memberships.enums import DataProductUserRole
@@ -60,6 +61,20 @@ class TestDataProductsRouter:
         assert response.status_code == 200
         assert response.json()["id"] == str(data_product.id)
 
+    def test_get_conveyor_notebook_url(self, client):
+        user = UserFactory(external_id="sub")
+        data_product = DataProductMembershipFactory(user=user).data_product
+
+        response = self.get_conveyor_url(client, data_product.id)
+        assert response.status_code == 501
+
+    def test_get_conveyor_ide_url(self, client):
+        user = UserFactory(external_id="sub")
+        data_product = DataProductMembershipFactory(user=user).data_product
+
+        response = self.get_conveyor_ide_url(client, data_product.id)
+        assert response.status_code == 501
+
     def test_get_data_product_by_user_id(self, client):
         user = UserFactory(external_id="sub")
         data_product = DataProductMembershipFactory(user=user).data_product
@@ -68,6 +83,15 @@ class TestDataProductsRouter:
         assert response.status_code == 200
         assert len(response.json()) == 1
         assert response.json()[0]["id"] == str(data_product.id)
+
+    def test_get_data_outputs(self, client):
+        user = UserFactory(external_id="sub")
+        data_product = DataProductMembershipFactory(user=user).data_product
+        data_output = DataOutputFactory(owner=data_product)
+        response = self.get_data_outputs(client, data_product.id)
+        assert response.status_code == 200
+        assert len(response.json()) == 1
+        assert response.json()[0]["id"] == str(data_output.id)
 
     def test_update_data_product_no_member(self, payload, client):
         data_product = DataProductFactory()
@@ -146,5 +170,17 @@ class TestDataProductsRouter:
         return client.get(f"{ENDPOINT}/{data_product_id}")
 
     @staticmethod
+    def get_data_outputs(client, data_product_id):
+        return client.get(f"{ENDPOINT}/{data_product_id}/data_outputs")
+
+    @staticmethod
     def get_data_product_by_user_id(client, user_id):
         return client.get(f"{ENDPOINT}/user/{user_id}")
+
+    @staticmethod
+    def get_conveyor_url(client, data_product_id):
+        return client.get(f"{ENDPOINT}/{data_product_id}/conveyor_notebook_url")
+
+    @staticmethod
+    def get_conveyor_ide_url(client, data_product_id):
+        return client.get(f"{ENDPOINT}/{data_product_id}/conveyor_ide_url")
