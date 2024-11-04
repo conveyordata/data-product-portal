14142ece734ef4f121b55bb3ab305634da7323a9
Mathias Bongaerts <mathiasbongaerts@hotmail.com>
2024-11-04 08:43:22 +0100

    Added new tag API endpoints

---
+++ backend/app/tags/router.py
@@ -0,0 +1,28 @@
+from uuid import UUID
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database.database import get_db_session
+from app.tags.schema import Tag as TagGet
+from app.tags.schema import TagCreate
+from app.tags.service import TagService
+
+router = APIRouter(prefix="/tags", tags=["tags"])
+
+
+@router.get("")
+def get_tags(db: Session = Depends(get_db_session)) -> list[TagGet]:
+    return TagService().get_tags(db)
+
+
+@router.post("")
+def create_tag(
+    tag: TagCreate, db: Session = Depends(get_db_session)
+) -> dict[str, UUID]:
+    return TagService().create_tag(tag, db)
+
+
+@router.delete("/{id}")
+def remove_tag(id: UUID, db: Session = Depends(get_db_session)):
+    return TagService().remove_tag(id, db)
---
+++ backend/app/tags/service.py
@@ -0,0 +1,25 @@
+from uuid import UUID
+
+from sqlalchemy import asc
+from sqlalchemy.orm import Session
+
+from app.tags.model import Tag as TagModel
+from app.tags.schema import Tag as TagGet
+from app.tags.schema import TagCreate
+
+
+class TagService:
+    def get_tags(self, db: Session) -> list[TagGet]:
+        return db.query(TagModel).order_by(asc(TagModel.value)).all()
+
+    def create_tag(self, tag: TagCreate, db: Session) -> dict[str, UUID]:
+        tag = TagModel(**tag.parse_pydantic_schema())
+        db.add(tag)
+        db.commit()
+
+        return {"id": tag.id}
+
+    def remove_tag(self, id: UUID, db: Session):
+        tag = db.get(TagModel, id)
+        tag.delete()
+        db.commit()
