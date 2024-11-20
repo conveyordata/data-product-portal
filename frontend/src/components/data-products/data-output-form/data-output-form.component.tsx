4e75248a4eb00f870fbeb235cf0183901e565ed6
Stijn Janssens <janssens.stijn@hotmail.com>
2024-11-20 11:16:07 +0100

    Feature/frontend improvements (#419)
    * Add under construction page, add tooltips
    
    * Add switch and explanations for source vs product aligned
    
    * Put dropdown and fix navigation on output creation
    
    * Fix padding issue with dropdowns in modal

--- frontend/src/components/data-products/data-output-form/data-output-form.component.tsx
+++ frontend/src/components/data-products/data-output-form/data-output-form.component.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Form, FormInstance, FormProps, Input, Space } from 'antd';
+import { Form, FormInstance, FormProps, Input, Select, Space } from 'antd';
 import { useTranslation } from 'react-i18next';
 import styles from './data-output-form.module.scss';
 import { useGetDataProductByIdQuery } from '@/store/features/data-products/data-products-api-slice.ts';
@@ -21,6 +21,7 @@ import { DataOutputStatus } from '@/types/data-output/data-output.contract';
 import { useGetAllPlatformsConfigsQuery } from '@/store/features/platform-service-configs/platform-service-configs-api-slice';
 import { DatabricksDataOutputForm } from './databricks-data-output-form.component';
 import { SnowflakeDataOutputForm } from './snowflake-data-output-form.component';
+import { TabKeys } from '@/pages/data-product/components/data-product-tabs/data-product-tabs';
 
 type Props = {
     mode: 'create';
@@ -84,7 +85,7 @@ export function DataOutputForm({ mode, formRef, dataProductId, modalCallbackOnSu
                 await createDataOutput(request).unwrap();
                 dispatchMessage({ content: t('Data output created successfully'), type: 'success' });
                 modalCallbackOnSubmit();
-                navigate(createDataProductIdPath(dataProductId));
+                navigate(createDataProductIdPath(dataProductId, TabKeys.DataOutputs));
 
                 form.resetFields();
             }
@@ -138,6 +139,7 @@ export function DataOutputForm({ mode, formRef, dataProductId, modalCallbackOnSu
             });
         }
     }, [dataProductNameValue]);
+    const options = [{label: t('Product aligned'), value: false}, {label: t('Source aligned'), value: true}]
 
     return (
         <Form
@@ -152,6 +154,7 @@ export function DataOutputForm({ mode, formRef, dataProductId, modalCallbackOnSu
             requiredMark={'optional'}
             labelWrap
             disabled={isLoading}
+            className={styles.form}
         >
             <Form.Item<DataOutputCreateFormSchema>
                 name={'name'}
@@ -185,8 +188,16 @@ export function DataOutputForm({ mode, formRef, dataProductId, modalCallbackOnSu
             </Form.Item>
             <Form.Item<DataOutputCreateFormSchema>
                 name={'is_source_aligned'} valuePropName="checked"
+                label={t('Alignment')}
+                required
+                tooltip={t('If you follow product thinking approach, select Product aligned. If you want more freedom, you can select Source aligned, however this request will need to be approved by administrators. By default, or when in doubt, leave product aligned selected.')}
             >
-                <Checkbox>{t('Source aligned')}</Checkbox>
+                <Select
+                    allowClear={false}
+                    defaultActiveFirstOption
+                    defaultValue={false}
+                    options={options}
+                />
             </Form.Item>
             <Form.Item>
                 <Space wrap className={styles.radioButtonContainer}>
