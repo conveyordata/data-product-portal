24cdaa67f1579285cd76d613ffa21d945f1a4d83
vlieven <lieven.verswyvel@gmail.com>
2025-01-24 18:11:00 +0100

    Finish for the day

--- frontend/src/pages/roles/components/roles-table.component.tsx
+++ frontend/src/pages/roles/components/roles-table.component.tsx
@@ -1,51 +1,80 @@
-import styles from '@/pages/platforms-configs/components/platforms-configs-table/platforms-configs-table.module.scss';
+import styles from './roles-table.module.scss';
 import { Flex, Table, Typography, Checkbox, type CheckboxChangeEvent, Button, Space, Tooltip } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { useCallback, useState } from 'react';
 import { InfoCircleOutlined } from '@ant-design/icons';
+import type { ColumnType } from 'antd/es/table/interface';
 
 const { Text } = Typography;
+
+type PermissionType = 'Group' | 'Instance';
+
+type PermissionInstance = {
+    type: PermissionType;
+    permission: string;
+    description: string;
+    access: Map<string, string>;
+};
+
+type PermissionGroup = {
+    type: PermissionType;
+    name: string;
+};
+
+type Permission = PermissionInstance | PermissionGroup;
 
 export function RolesTable() {
     const { t } = useTranslation();
 
-    const [data, setData] = useState([
+    const [data, setData] = useState<Permission[]>([
         {
-            key: '1',
+            type: 'Group',
+            name: 'User Management',
+        },
+        {
+            type: 'Instance',
             permission: 'View User Management Table',
+            description: 'Interesting info on user management',
             access: {
                 'Super User': true,
-                'Superintendent': true,
+                Superintendent: true,
                 'Device Manager': true,
                 'Financial Executive': true,
             },
         },
         {
-            key: '2',
+            type: 'Instance',
             permission: 'Bulk Edit',
+            description: 'Interesting info on bulking',
             access: {
                 'Super User': true,
-                'Superintendent': false,
+                Superintendent: false,
                 'Device Manager': false,
                 'Financial Executive': false,
             },
         },
         {
-            key: '3',
+            type: 'Instance',
             permission: 'Log In As',
+            description: 'Interesting info on logging in',
             access: {
                 'Super User': true,
-                'Superintendent': true,
+                Superintendent: true,
                 'Device Manager': false,
                 'Financial Executive': false,
             },
         },
         {
-            key: '4',
+            type: 'Group',
+            name: 'Role Management',
+        },
+        {
+            type: 'Instance',
             permission: 'Role Management',
+            description: 'Interesting info on managing roles',
             access: {
                 'Super User': true,
-                'Superintendent': true,
+                Superintendent: true,
                 'Device Manager': true,
                 'Financial Executive': false,
             },
@@ -53,13 +82,13 @@ export function RolesTable() {
     ]);
 
     const handleCheckboxChange = useCallback(
-        (record, key: string, value: boolean) => {
+        (record: PermissionInstance, key: string, value: boolean) => {
             const updatedData = data.map((item) => {
-                if (item.key === record.key) {
+                if (item.type === 'Instance' && (item as PermissionInstance).permission === record.permission) {
                     return {
                         ...item,
                         access: {
-                            ...item.access,
+                            ...(item as PermissionInstance).access,
                             [key]: value,
                         },
                     };
@@ -72,18 +101,43 @@ export function RolesTable() {
         [data],
     );
 
-    const renderCheckbox = (key: string) => (value: boolean, record) => {
-        return (
-            <Flex justify={'center'}>
-                <Checkbox
-                    checked={value}
-                    onChange={(e: CheckboxChangeEvent) => handleCheckboxChange(record, key, e.target.checked)}
-                />
-            </Flex>
-        );
+    const renderPermission = (_: string, record: Permission) => {
+        if (record.type === 'Instance') {
+            record = record as PermissionInstance;
+            return (
+                <Space className={styles.permissionInstance}>
+                    <Text>{record.permission}</Text>
+                    <Tooltip title={record.description}>
+                        <InfoCircleOutlined />
+                    </Tooltip>
+                </Space>
+            );
+        } else if (record.type === 'Group') {
+            record = record as PermissionGroup;
+            return (
+                <Space className={styles.permissionGroup}>
+                    <Text strong>{record.name}</Text>
+                </Space>
+            );
+        }
     };
 
-    const createColumn = (title: string, description: string) => {
+    const renderCheckbox = (key: string) => (value: boolean, record: Permission) => {
+        if (record.type === 'Instance') {
+            return (
+                <Flex justify={'center'}>
+                    <Checkbox
+                        checked={value}
+                        onChange={(e: CheckboxChangeEvent) =>
+                            handleCheckboxChange(record as PermissionInstance, key, e.target.checked)
+                        }
+                    />
+                </Flex>
+            );
+        }
+    };
+
+    const createColumn = (title: string, description: string): ColumnType<Permission> => {
         return {
             title: (
                 <Space>
@@ -94,7 +148,6 @@ export function RolesTable() {
                 </Space>
             ),
             dataIndex: ['access', title],
-            key: title,
             render: renderCheckbox(title),
         };
     };
@@ -106,11 +159,12 @@ export function RolesTable() {
         { title: 'Financial Executive', description: 'The financial executive' },
     ].map((item) => createColumn(item.title, item.description));
 
-    const columns = [
+    const columns: ColumnType<Permission>[] = [
         {
-            title: 'Permission',
             dataIndex: 'permission',
-            key: 'permission',
+            fixed: 'left',
+            className: styles.permissionsColumn,
+            render: renderPermission,
         },
         ...roleColumns,
     ];
@@ -118,13 +172,13 @@ export function RolesTable() {
     return (
         <Flex vertical className={styles.tableContainer}>
             <Flex className={styles.searchContainer}>
-                <Typography.Title level={3}>{t('Roles')}</Typography.Title>
+                <Typography.Title level={3}>{t('Global roles')}</Typography.Title>
                 <Button className={styles.formButton} type={'primary'}>
-                    {t('Create new role')}
+                    {t('Create new global role')}
                 </Button>
             </Flex>
             <Flex vertical className={styles.tableFilters}>
-                <Table columns={columns} dataSource={data} />
+                <Table columns={columns} dataSource={data} pagination={false} rowKey={'permission'} />
             </Flex>
         </Flex>
     );
--- frontend/src/pages/roles/roles.page.tsx
+++ frontend/src/pages/roles/roles.page.tsx
@@ -1,9 +1,15 @@
 import styles from './roles.module.scss';
 import { RolesTable } from '@/pages/roles/components/roles-table.component.tsx';
+import { Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 export function RoleConfiguration() {
+    const { t } = useTranslation();
+
     return (
         <div className={styles.container}>
+            <Typography.Title level={2}>{t('Manage Roles')}</Typography.Title>
+
             <RolesTable />
         </div>
     );
