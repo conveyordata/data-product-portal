c5bd53a0d6156d6028f32f47c6f84711c4b1e460
vlieven <lieven.verswyvel@gmail.com>
2025-02-13 16:41:12 +0100

    Probably makes no difference

--- frontend/src/components/layout/public/public.layout.tsx
+++ frontend/src/components/layout/public/public.layout.tsx
@@ -1,11 +1,23 @@
-import React, { type ReactNode } from 'react';
+import React, { type ReactNode, useEffect } from 'react';
 import { Layout, theme } from 'antd';
 import styles from './public.module.scss';
+import { useAuth } from 'react-oidc-context';
+import { useNavigate } from 'react-router-dom';
+import { ApplicationPaths } from '@/types/navigation.ts';
 
 const PublicLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
+
+    const { isAuthenticated } = useAuth();
+    const redirect = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            redirect(ApplicationPaths.Home);
+        }
+    }, [isAuthenticated, redirect]);
 
     return (
         <Layout className={styles.layoutWrapper} hasSider>
