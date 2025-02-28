c07c9bb3fbba4d3b2902e0276e15bcda244a25ec
Stijn Janssens <janssens.stijn@hotmail.com>
2025-02-28 15:52:34 +0100

    Missing data product unit tests

--- backend/tests/data_products/test_router.py
+++ backend/tests/data_products/test_router.py
@@ -11,6 +11,7 @@ from tests.factories import (
 from tests.factories.data_output import DataOutputFactory
 from tests.factories.data_product_membership import DataProductMembershipFactory
 from tests.factories.data_product_setting import DataProductSettingFactory
+from tests.factories.environment import EnvironmentFactory
 from tests.factories.lifecycle import LifecycleFactory
 
 from app.data_product_memberships.enums import DataProductUserRole
@@ -133,6 +134,25 @@ class TestDataProductsRouter:
         response = self.delete_data_product(client, data_product.id)
         assert response.status_code == 200
 
+    def test_update_status_not_owner(self, client):
+        data_product = DataProductFactory()
+        response = self.update_data_product_status(
+            client, {"status": "active"}, data_product.id
+        )
+        assert response.status_code == 403
+
+    def test_update_status(self, client):
+        data_product = DataProductMembershipFactory(
+            user=UserFactory(external_id="sub")
+        ).data_product
+        response = self.get_data_product_by_id(client, data_product.id)
+        assert response.json()["status"] == "pending"
+        response = self.update_data_product_status(
+            client, {"status": "active"}, data_product.id
+        )
+        response = self.get_data_product_by_id(client, data_product.id)
+        assert response.json()["status"] == "active"
+
     def test_get_data_product_by_id_with_invalid_id(self, client):
         data_product = self.get_data_product_by_id(client, self.invalid_id)
         assert data_product.status_code == 404
@@ -173,6 +193,45 @@ class TestDataProductsRouter:
         response = client.get(f"{ENDPOINT}/{data_product.id}")
         assert response.json()["data_product_settings"][0]["value"] == "false"
 
+    def test_get_graph_data(self, client):
+        data_product = DataProductFactory()
+        response = client.get(f"{ENDPOINT}/{data_product.id}/graph")
+        assert response.json()["edges"] == []
+        for node in response.json()["nodes"]:
+            assert node == {
+                "data": {
+                    "icon_key": "default",
+                    "id": str(data_product.id),
+                    "link_to_id": None,
+                    "name": data_product.name,
+                },
+                "id": str(data_product.id),
+                "isMain": True,
+                "type": "dataProductNode",
+            }
+
+    def test_get_aws_role_not_member(self, client):
+        data_product = DataProductFactory()
+        response = client.get(f"{ENDPOINT}/{data_product.id}/role")
+        assert response.status_code == 403
+
+    def test_get_aws_role(self, client):
+        env = EnvironmentFactory(name="production")
+        data_product = DataProductMembershipFactory(
+            user=UserFactory(external_id="sub")
+        ).data_product
+        response = client.get(
+            f"{ENDPOINT}/{data_product.id}/role?environment=production"
+        )
+        assert response.status_code == 200
+        assert response.json() == env.context.replace("{{}}", data_product.external_id)
+
+    def test_get_signin_url(self, client):
+        pass
+
+    def test_get_databricks_url(self, client):
+        pass
+
     @staticmethod
     def create_data_product(client, default_data_product_payload):
         return client.post(ENDPOINT, json=default_data_product_payload)
@@ -185,6 +244,10 @@ class TestDataProductsRouter:
     def update_data_product_about(client, data_product_id):
         data = {"about": "Updated Data Product Description"}
         return client.put(f"{ENDPOINT}/{data_product_id}/about", json=data)
+
+    @staticmethod
+    def update_data_product_status(client, status, data_product_id):
+        return client.put(f"{ENDPOINT}/{data_product_id}/status", json=status)
 
     @staticmethod
     def delete_data_product(client, data_product_id):
