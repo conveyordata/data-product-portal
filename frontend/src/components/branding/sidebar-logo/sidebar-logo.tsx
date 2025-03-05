9981eefb55d0ce722f3d69fc8cf7ea6cf7fd6420
Mathias Bongaerts <mathiasbongaerts@hotmail.com>
2025-03-05 15:09:12 +0100

    Added fallback for portal name in frontend Also added a missing translation entry

--- frontend/src/components/branding/sidebar-logo/sidebar-logo.tsx
+++ frontend/src/components/branding/sidebar-logo/sidebar-logo.tsx
@@ -1,5 +1,6 @@
 import { Flex, Typography } from 'antd';
 import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
 
 import { useGetGeneralSettingsQuery } from '@/store/features/general-settings/general-settings-api-slice';
 
@@ -10,11 +11,12 @@ type Props = {
 };
 
 export function SidebarLogo({ variant = 'light' }: Props) {
+    const { t } = useTranslation();
     const { data: settings } = useGetGeneralSettingsQuery();
     return (
         <Flex vertical className={styles.logoWrapper}>
             <Typography.Text className={clsx(styles.logoText, styles[variant])}>
-                {settings?.portal_name}
+                {settings ? settings.portal_name : t('Data Product Portal')}
             </Typography.Text>
         </Flex>
     );
