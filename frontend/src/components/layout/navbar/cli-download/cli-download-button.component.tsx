60bae434354646e46c3dabd5cbf8b9c0f6375476
Mathias Bongaerts <mathiasbongaerts@hotmail.com>
2025-03-06 11:50:39 +0100

    General improvements - Better handling of undefined version - Some renamings and code structure changes

--- frontend/src/components/layout/navbar/cli-download/cli-download-button.component.tsx
+++ frontend/src/components/layout/navbar/cli-download/cli-download-button.component.tsx
@@ -1,71 +1,78 @@
 import { useGetVersionQuery } from '@/store/features/version/version-api-slice';
 import { WindowsOutlined, AppleOutlined, LinuxOutlined, DownloadOutlined } from '@ant-design/icons';
 import { Button, Dropdown } from 'antd';
-import { useTranslation } from 'react-i18next';
 import styles from './cli-download-button.module.scss';
+import { JSX } from 'react';
+
+type DownloadItem = {
+    label: string;
+    suffix: string;
+    icon: JSX.Element;
+};
+
+const createDownloadLink = (suffix: string, version: string) => {
+    return `https://github.com/conveyordata/data-product-portal/releases/download/v${version}/data-product-portal_${suffix}`;
+};
+
+const downloadMenuItem = (item: DownloadItem, version: string) => {
+    return {
+        label: (
+            <a href={createDownloadLink(item.suffix, version)} target="_blank">
+                {item.label}
+            </a>
+        ),
+        key: item.suffix,
+        icon: item.icon,
+    };
+};
+
+const WINDOWS_DOWNLOAD_ITEMS: DownloadItem[] = [
+    { label: 'X86/X64', suffix: 'Windows_x86_64.zip' },
+    { label: 'ARM64', suffix: 'Windows_arm64.zip' },
+    { label: 'i386', suffix: 'Windows_i386.zip' },
+].map((item) => ({
+    ...item,
+    icon: <WindowsOutlined />,
+}));
+
+const MAC_DOWNLOAD_ITEMS: DownloadItem[] = [
+    { label: 'X86/X64', suffix: 'Darwin_x86_64.tar.gz' },
+    { label: 'ARM64', suffix: 'Darwin_arm64.tar.gz' },
+].map((item) => ({
+    ...item,
+    icon: <AppleOutlined />,
+}));
+
+const LINUX_DOWNLOAD_ITEMS: DownloadItem[] = [
+    { label: 'X86/X64', suffix: 'Linux_x86_64.tar.gz' },
+    { label: 'ARM64', suffix: 'Linux_arm64.tar.gz' },
+    { label: 'i386', suffix: 'Linux_i386.tar.gz' },
+].map((item) => ({
+    ...item,
+    icon: <LinuxOutlined />,
+}));
+
+const isWindows = navigator.userAgent.includes('Win');
+const isMac = navigator.userAgent.includes('Mac');
+const isLinux = navigator.userAgent.includes('Linux');
+
+const downloadItems = isWindows
+    ? WINDOWS_DOWNLOAD_ITEMS
+    : isMac
+      ? MAC_DOWNLOAD_ITEMS
+      : isLinux
+        ? LINUX_DOWNLOAD_ITEMS
+        : WINDOWS_DOWNLOAD_ITEMS.concat(MAC_DOWNLOAD_ITEMS).concat(LINUX_DOWNLOAD_ITEMS);
 
 export const DownloadCLIButton = () => {
     const { data: version } = useGetVersionQuery();
-    const { t } = useTranslation();
-
-    type DownloadLink = {
-        label: string;
-        suffix: string;
-        icon: JSX.Element;
-    };
-
-    const windowsLinks: DownloadLink[] = [
-        { label: 'X86/X64', suffix: 'Windows_x86_64.zip', icon: <WindowsOutlined /> },
-        { label: 'ARM64', suffix: 'Windows_arm64.zip', icon: <WindowsOutlined /> },
-        { label: 'i386', suffix: 'Windows_i386.zip', icon: <WindowsOutlined /> },
-    ];
-    const macLinks: DownloadLink[] = [
-        { label: 'X86/X64', suffix: 'Darwin_x86_64.tar.gz', icon: <AppleOutlined /> },
-        { label: 'ARM64', suffix: 'Darwin_arm64.tar.gz', icon: <AppleOutlined /> },
-    ];
-    const linuxLinks: DownloadLink[] = [
-        { label: 'X86/X64', suffix: 'Linux_x86_64.tar.gz' },
-        { label: 'ARM64', suffix: 'Linux_arm64.tar.gz' },
-        { label: 'i386', suffix: 'Linux_i386.tar.gz' },
-    ].map((link) => ({
-        ...link,
-        icon: <LinuxOutlined />,
-    }));
-
-    const downloadLink = (suffix: string, version: string) => {
-        return `https://github.com/conveyordata/data-product-portal/releases/download/v${version}/data-product-portal_${suffix}`;
-    };
-
-    const item = (link: DownloadLink, version: string) => {
-        return {
-            label: (
-                <a href={downloadLink(link.suffix, version)} target="_blank">
-                    {link.label}
-                </a>
-            ),
-            key: link.suffix,
-            icon: link.icon,
-        };
-    };
-
-    const isWindows = navigator.userAgent.includes('Win');
-    const isMac = navigator.userAgent.includes('Mac');
-    const isLinux = navigator.userAgent.includes('Linux');
-
-    const menuLinks = isWindows
-        ? windowsLinks
-        : isMac
-          ? macLinks
-          : isLinux
-            ? linuxLinks
-            : windowsLinks.concat(macLinks).concat(linuxLinks);
 
     const menuProps = {
-        items: menuLinks.map((link) => item(link, version?.version || 'latest')),
+        items: version ? downloadItems.map((item) => downloadMenuItem(item, version.version)) : [],
     };
 
     return (
-        <Dropdown menu={menuProps} trigger={['click']} placement={'bottomRight'}>
+        <Dropdown menu={menuProps} trigger={['click']} placement={'bottomRight'} disabled={!version}>
             <Button shape={'circle'} className={styles.iconButton} icon={<DownloadOutlined />} />
         </Dropdown>
     );
