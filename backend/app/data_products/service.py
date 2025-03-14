7eda6872d3b8e55012a35913e45de5415ea7e1f6
Tuur Nieman <tuur.nieman@dataminded.com>
2025-03-14 15:42:53 +0100

    notifications on data product dataset creation

--- backend/app/data_products/service.py
+++ backend/app/data_products/service.py
@@ -391,7 +391,8 @@ class DataProductService:
         else:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail=f"Unsupported status for DataOutPutDatasetStatus when creating link between data product and dataset.",
+                detail=
+                "Unsupported status for DataOutPutDatasetStatus when creating link between data product and dataset."
             )
 
         db.commit()
