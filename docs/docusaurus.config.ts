c5095d98d08354105a61298472c150b62cc8c52a
Stijn Janssens <janssens.stijn@hotmail.com>
2025-04-07 11:29:18 +0200

    Added Concepts page

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -30,7 +30,10 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+  markdown: {
+    mermaid: true
+  },
+  themes: ['@docusaurus/theme-mermaid'],
   presets: [
     [
       'classic',
