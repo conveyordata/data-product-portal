6b574f2bb39dd0dcc5c195b871403d2a6057d057
Stijn Janssens <janssens.stijn@hotmail.com>
2025-04-07 15:15:10 +0200

    Remove obsolete log

--- frontend/src/store/features/authorization/authorization-api-slice.ts
+++ frontend/src/store/features/authorization/authorization-api-slice.ts
@@ -5,16 +5,11 @@ import type { AccessRequest, AccessResponse } from '@/types/authorization';
 const authorizationApiSlice = baseApiSlice.injectEndpoints({
     endpoints: (builder) => ({
         checkAccess: builder.query<AccessResponse, AccessRequest>({
-            query: (access_request) => {
-                // Log the full access_request object
-                console.log('Access Request:', access_request);
-
-                return {
-                    url: ApiUrl.AccessCheck,
-                    method: 'POST',
-                    data: access_request,
-                };
-            },
+            query: (access_request) => ({
+                url: ApiUrl.AccessCheck,
+                method: 'POST',
+                data: access_request,
+            }),
         }),
     }),
 });
