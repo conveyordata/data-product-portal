0d1094663cb2f659ca1651a814469526ea248b4e
Stijn Janssens <janssens.stijn@hotmail.com>
2025-04-07 16:20:00 +0200

    Added user guide docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/conveyordata/data-product-portal/tree/main/docs/',
         },
         blog: {
           showReadingTime: true,
