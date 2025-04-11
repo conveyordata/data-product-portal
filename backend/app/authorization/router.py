c6027ff3ec4bc683be0615b683e8a9bdb7fb874e
vlieven <lieven.verswyvel@gmail.com>
2025-04-11 13:24:15 +0200

    Change access endpoint to GET

--- backend/app/authorization/router.py
+++ backend/app/authorization/router.py
@@ -1,15 +1,19 @@
+from typing import Optional
+from uuid import UUID
+
 from fastapi import APIRouter, Depends
 
-from app.authorization.schema import AccessRequest, AccessResponse
+from app.authorization.schema import AccessResponse
 from app.core.auth.auth import get_authenticated_user
+from app.core.authz.actions import AuthorizationAction
 from app.core.authz.authorization import Authorization
 from app.users.schema import User
 
 router = APIRouter(prefix="/authz", tags=["authz"])
 
 
-@router.post(
-    "/access/check",
+@router.get(
+    "/access/{action}",
     responses={
         200: {
             "description": "Access check result",
@@ -20,15 +24,18 @@ router = APIRouter(prefix="/authz", tags=["authz"])
     },
 )
 def check_access(
-    request: AccessRequest, user: User = Depends(get_authenticated_user)
+    action: AuthorizationAction,
+    resource: Optional[UUID] = None,
+    domain: Optional[UUID] = None,
+    user: User = Depends(get_authenticated_user),
 ) -> AccessResponse:
     """Allows the requesting user to check whether an access check will fail or succeed.
     Useful to conditionally disable parts of the UI that are known to be inaccessible.
     """
     sub = str(user.id)
-    dom = "*" if request.domain is None else str(request.domain)
-    obj = "*" if request.object_id is None else str(request.object_id)
+    dom = "*" if domain is None else str(domain)
+    obj = "*" if resource is None else str(resource)
 
     authorizer = Authorization()
-    access = authorizer.has_access(sub=sub, dom=dom, obj=obj, act=request.action)
-    return AccessResponse(access=access)
+    result = authorizer.has_access(sub=sub, dom=dom, obj=obj, act=action)
+    return AccessResponse(allowed=result)
