7328e76cc16ba50456b979af3e84ddb55e7adf88
Stijn Janssens <janssens.stijn@hotmail.com>
2025-04-22 13:20:18 +0200

    Open docs in new tab

--- frontend/src/components/layout/sidebar/sidebar.component.tsx
+++ frontend/src/components/layout/sidebar/sidebar.component.tsx
@@ -44,6 +44,7 @@ export const Sidebar = () => {
             label: (
                 <a
                     href={`https://d33vpinjygaq6n.cloudfront.net/docs/${version?.version.split('.').slice(0, 2).join('.')}.x/intro`}
+                    target="_blank"
                 >
                     {t('Documentation')}
                 </a>
