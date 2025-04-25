ed12b26908552672903192e2e290339c28151ea6
Tuur Nieman <tuur.nieman@dataminded.com>
2025-04-25 12:26:49 +0200

    UI

--- frontend/src/pages/home/components/pending-requests-inbox/pending-requests-list.tsx
+++ frontend/src/pages/home/components/pending-requests-inbox/pending-requests-list.tsx
@@ -1,5 +1,5 @@
 import { UserOutlined } from '@ant-design/icons';
-import { Avatar, Button, Flex, List, Typography } from 'antd';
+import { Avatar, Button, Flex, List, Space, Typography } from 'antd';
 import type { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
@@ -61,57 +61,54 @@ export const PendingRequestsList = ({ pendingActionItems, pagination, onAccept, 
                             className={styles.listItem}
                             onClick={() => handleItemClick(item.navigatePath)}
                         >
-                            <List.Item.Meta
-                                className={styles.itemCard}
-                                avatar={
+                            <Flex vertical className={styles.itemCard} gap={4}>
+                                <Flex align="center">
                                     <Avatar
                                         style={{ backgroundColor: item.color }}
-                                        size="large"
                                         className={styles.avatar}
+                                        size="large"
                                     >
                                         {item.initials || <UserOutlined />}
                                     </Avatar>
-                                }
-                                title={
-                                    <Flex vertical gap={2}>
+                                    <Flex vertical>
                                         <Typography.Text>{item.description}</Typography.Text>
                                         <Typography.Text type="secondary" style={{ fontWeight: 'normal' }}>
                                             by {item.author}, {formattedDate}
                                         </Typography.Text>
                                     </Flex>
-                                }
-                                description={<Typography.Text>{item.message}</Typography.Text>}
-                            />
-                            <Flex align="end" className={styles.actionsTopRight}>
-                                <Flex className={styles.typeIndicator} gap="small">
-                                    <Typography.Text className={styles.tag} type="secondary">
-                                        {item.icon}
-                                    </Typography.Text>
-                                    <Typography.Text>{item.origin}</Typography.Text>
-                                </Flex>
+                                    <Flex align="flex-start" justify="flex-start" className={styles.endActions}>
+                                        <Flex className={styles.typeIndicator} gap="small">
+                                            <Typography.Text className={styles.tag} type="secondary">
+                                                {item.icon}
+                                            </Typography.Text>
+                                            <Typography.Text>{item.origin}</Typography.Text>
+                                        </Flex>
 
-                                <div className={styles.buttons}>
-                                    <Button
-                                        onClick={(e) => {
-                                            e.stopPropagation();
-                                            onAccept(item.request);
-                                        }}
-                                        className={styles.button}
-                                        type="link"
-                                    >
-                                        {t('Accept')}
-                                    </Button>
-                                    <Button
-                                        onClick={(e) => {
-                                            e.stopPropagation();
-                                            onReject(item.request);
-                                        }}
-                                        className={styles.button}
-                                        type="link"
-                                    >
-                                        {t('Reject')}
-                                    </Button>
-                                </div>
+                                        <div className={styles.buttons}>
+                                            <Button
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    onAccept(item.request);
+                                                }}
+                                                className={styles.button}
+                                                type="link"
+                                            >
+                                                {t('Accept')}
+                                            </Button>
+                                            <Button
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    onReject(item.request);
+                                                }}
+                                                className={styles.button}
+                                                type="link"
+                                            >
+                                                {t('Reject')}
+                                            </Button>
+                                        </div>
+                                    </Flex>
+                                </Flex>
+                                <Typography.Text className={styles.message}>{item.message}</Typography.Text>
                             </Flex>
                         </List.Item>
                         <div />
