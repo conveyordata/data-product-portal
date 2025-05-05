0f916e019530f19bbc0e81a2fa941562b8953b1f
Tuur Nieman <tuur.nieman@dataminded.com>
2025-05-05 13:25:30 +0200

    improved enum handling in alembic script

--- backend/app/database/alembic/versions/2025_05_02_1453-e7aa46492f9a_notifications_tables.py
+++ backend/app/database/alembic/versions/2025_05_02_1453-e7aa46492f9a_notifications_tables.py
@@ -10,10 +10,9 @@ from typing import Sequence, Union
 
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import ENUM, UUID
 
 from app.notifications.enums import NotificationTypes
-from app.role_assignments.enums import DecisionStatus
 from app.shared.model import utcnow
 
 # revision identifiers, used by Alembic.
@@ -24,12 +23,17 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    # Create the "notifications" table
+
+    decision_status = ENUM(name="decisionstatus", create_type=False)
+
     op.create_table(
         "notifications",
         sa.Column("id", UUID(as_uuid=True), primary_key=True),
-        sa.Column("notification_type", sa.Enum(NotificationTypes)),
-        sa.Column("notification_origin", sa.Enum(DecisionStatus)),
+        sa.Column(
+            "notification_type",
+            sa.Enum(NotificationTypes, name="notificationtypes", create_type=True),
+        ),
+        sa.Column("notification_origin", decision_status, nullable=False),
         sa.Column("data_product_dataset_id", sa.UUID),
         sa.Column("data_output_dataset_id", sa.UUID),
         sa.Column("data_product_membership_id", sa.UUID),
@@ -52,3 +56,4 @@ def upgrade() -> None:
 
 def downgrade() -> None:
     op.drop_table("notifications")
+    op.execute("DROP TYPE notificationtypes")
