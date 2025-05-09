1110afe882fa9d700b677d1f8a819d0b94012879
Tuur Nieman <tuur.nieman@dataminded.com>
2025-05-09 14:36:29 +0200

    tab design cleanup to match other tabs

--- frontend/src/components/history/history-tab.tsx
+++ frontend/src/components/history/history-tab.tsx
@@ -5,18 +5,11 @@ import { TFunction } from 'i18next';
 import { useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { EmptyList } from '@/components/empty/empty-list/empty-list.component.tsx';
 import { TABLE_SUBSECTION_PAGINATION } from '@/constants/table.constants';
 import { useTablePagination } from '@/hooks/use-table-pagination';
-import {
-    useGetDataOutputByIdQuery,
-    useGetDataOutputHistoryQuery,
-} from '@/store/features/data-outputs/data-outputs-api-slice';
-import {
-    useGetDataProductByIdQuery,
-    useGetDataProductHistoryQuery,
-} from '@/store/features/data-products/data-products-api-slice';
-import { useGetDatasetByIdQuery, useGetDatasetHistoryQuery } from '@/store/features/datasets/datasets-api-slice';
+import { useGetDataOutputHistoryQuery } from '@/store/features/data-outputs/data-outputs-api-slice';
+import { useGetDataProductHistoryQuery } from '@/store/features/data-products/data-products-api-slice';
+import { useGetDatasetHistoryQuery } from '@/store/features/datasets/datasets-api-slice';
 import { EventContract } from '@/types/events/event.contract';
 import { getSubjectDisplayLabel, getTargetDisplayLabel } from '@/utils/history.helper';
 
@@ -49,34 +42,36 @@ function filterHistory(events: EventContract[], searchTerm: string, t: TFunction
 }
 
 export function HistoryTab({ id, type }: Props) {
-    const dataProductQuery = useGetDataProductByIdQuery(id, { skip: type !== 'dataproduct' || !id });
-    const datasetQuery = useGetDatasetByIdQuery(id, { skip: type !== 'dataset' || !id });
-    const dataOutputQuery = useGetDataOutputByIdQuery(id, { skip: type !== 'dataoutput' || !id });
-    const dataOutputHistoryQuery = useGetDataOutputHistoryQuery(id, { skip: !id });
-    const dataProductHistoryQuery = useGetDataProductHistoryQuery(id, { skip: !id });
-    const datasetHistoryQuery = useGetDatasetHistoryQuery(id, { skip: !id });
+    const { data: dataOutputHistoryData, isLoading: isFetchingDataOutputHistory } = useGetDataOutputHistoryQuery(id, {
+        skip: !id,
+    });
+    const { data: dataProductHistoryData, isLoading: isFetchingDataProductHistory } = useGetDataProductHistoryQuery(
+        id,
+        { skip: !id },
+    );
+    const { data: datasetHistoryData, isLoading: isFetchingDatasetHistory } = useGetDatasetHistoryQuery(id, {
+        skip: !id,
+    });
 
-    let dataQuery;
-    let historyQuery;
+    let history;
+    let isFetching;
 
     switch (type) {
         case 'dataproduct':
-            dataQuery = dataProductQuery;
-            historyQuery = dataProductHistoryQuery;
+            history = dataProductHistoryData;
+            isFetching = isFetchingDataProductHistory;
             break;
         case 'dataset':
-            dataQuery = datasetQuery;
-            historyQuery = datasetHistoryQuery;
+            history = datasetHistoryData;
+            isFetching = isFetchingDatasetHistory;
             break;
         case 'dataoutput':
-            dataQuery = dataOutputQuery;
-            historyQuery = dataOutputHistoryQuery;
+            history = dataOutputHistoryData;
+            isFetching = isFetchingDataOutputHistory;
             break;
     }
 
     const { t } = useTranslation();
-    const { data: data } = dataQuery;
-    const { data: history } = historyQuery;
     const [searchForm] = Form.useForm<SearchForm>();
     const searchTerm = Form.useWatch('search', searchForm);
 
@@ -96,7 +91,6 @@ export function HistoryTab({ id, type }: Props) {
         resetPagination();
     }, [filteredHistory, resetPagination]);
 
-    // Define table columns
     const columns = [
         {
             title: t('Event name'),
@@ -145,27 +139,25 @@ export function HistoryTab({ id, type }: Props) {
                 formItemProps={{ initialValue: '', className: styles.marginBottomLarge }}
                 placeholder={t('Search event history')}
             />
-            {filteredHistory && filteredHistory.length > 0 ? (
-                <Table<EventContract>
-                    dataSource={filteredHistory.map((item, index) => ({ ...item, key: index }))}
-                    columns={columns}
-                    onChange={onChange}
-                    pagination={{
-                        ...pagination,
-                        position: ['topRight'],
-                        size: 'small',
-                        showTotal: (total, range) =>
-                            t('Showing {{range0}}-{{range1}} of {{total}} history items', {
-                                range0: range[0],
-                                range1: range[1],
-                                total: total,
-                            }),
-                        className: styles.pagination,
-                    }}
-                />
-            ) : (
-                <EmptyList description={t(`No history available for {{name}}`, { name: data?.name })} />
-            )}
+            <Table<EventContract>
+                loading={isFetching}
+                dataSource={filteredHistory.map((item, index) => ({ ...item, key: index }))}
+                columns={columns}
+                onChange={onChange}
+                pagination={{
+                    ...pagination,
+                    position: ['topRight'],
+                    size: 'small',
+                    showTotal: (total, range) =>
+                        t('Showing {{range0}}-{{range1}} of {{total}} history items', {
+                            range0: range[0],
+                            range1: range[1],
+                            total: total,
+                        }),
+                    className: styles.pagination,
+                }}
+                size={'small'}
+            />
         </Flex>
     );
 }
