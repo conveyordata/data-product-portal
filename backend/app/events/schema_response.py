715040b7ac119cb0400b5bd7480479e5628d9f82
Tuur Nieman <tuur.nieman@dataminded.com>
2025-05-21 10:28:15 +0200

    small tweak

--- backend/app/events/schema_response.py
+++ backend/app/events/schema_response.py
@@ -3,9 +3,9 @@ from uuid import UUID
 
 from pydantic import NaiveDatetime
 
-from app.data_outputs.schema_response import BaseDataOutputGet
-from app.data_products.schema_response import BaseDataProductGet
-from app.datasets.schema_response import BaseDatasetGet
+from app.data_outputs.schema import DataOutput
+from app.data_products.schema import DataProduct
+from app.datasets.schema import Dataset
 from app.events.enum import EventReferenceEntity
 from app.shared.schema import ORMModel
 from app.users.schema import User
@@ -26,7 +26,7 @@ class EventGet(BaseEventGet):
     deleted_subject_identifier: Optional[str] = None
     deleted_target_identifier: Optional[str] = None
     actor: User
-    data_product: Optional[BaseDataProductGet] = None
+    data_product: Optional[DataProduct] = None
     user: Optional[User] = None
-    dataset: Optional[BaseDatasetGet] = None
-    data_output: Optional[BaseDataOutputGet] = None
+    dataset: Optional[Dataset] = None
+    data_output: Optional[DataOutput] = None
