8736990df6ac207a2b5b1fc74dcd04044635460e
Lowie Debois <lowie.debois@dataminded.com>
2025-07-02 16:01:51 +0200

    Added Data tracking enable/disable switch

--- frontend/src/pages/settings/components/settings-tabs/general-tab/general-tab.component.tsx
+++ frontend/src/pages/settings/components/settings-tabs/general-tab/general-tab.component.tsx
@@ -1,9 +1,15 @@
 import { ThemeSettingsForm } from '../components/theme-form/theme-form.component';
+import { DataTracking } from '../components/data-tracking/data-tracking.component';
 
 export function GeneralTab() {
     return (
         <div>
-            <ThemeSettingsForm />
+            <div>
+                <ThemeSettingsForm />
+            </div>
+            <div>
+                <DataTracking />
+            </div>
         </div>
     );
 }
