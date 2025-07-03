2e3a240807892c1125e5f1b75637aa9f4c1183c3
Lowie Debois <lowie.debois@dataminded.com>
2025-07-03 09:48:08 +0200

    Revert "Added Data tracking enable/disable switch"
    This reverts commit 8736990df6ac207a2b5b1fc74dcd04044635460e.

--- frontend/src/pages/settings/components/settings-tabs/general-tab/general-tab.component.tsx
+++ frontend/src/pages/settings/components/settings-tabs/general-tab/general-tab.component.tsx
@@ -1,15 +1,9 @@
-import { DataTracking } from '../components/data-tracking/data-tracking.component';
 import { ThemeSettingsForm } from '../components/theme-form/theme-form.component';
 
 export function GeneralTab() {
     return (
         <div>
-            <div>
-                <ThemeSettingsForm />
-            </div>
-            <div>
-                <DataTracking />
-            </div>
+            <ThemeSettingsForm />
         </div>
     );
 }
