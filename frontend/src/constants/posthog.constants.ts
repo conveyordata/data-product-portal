d31478f50c92ccecabb99aa96d8610e4d5f7160c
Lowie Debois <lowie.debois@dataminded.com>
2025-07-07 10:32:24 +0200

    Added data products events

--- frontend/src/constants/posthog.constants.ts
+++ frontend/src/constants/posthog.constants.ts
@@ -5,5 +5,9 @@ export enum PosthogEvents {
     MARKETPLACE_DATASET_TAB_CLICKED = 'marketplace_dataset_tab_clicked',
     MARKETPLACE_FILTER_USED = 'marketplace_filter_used',
 
+    DATA_PRODUCTS_TAB_CLICKED = 'data_products_tab_clicked',
     DATA_PRODUCTS_FILTER_USED = 'data_products_filter_used',
+    DATA_PRODUCTS_PLATFORM_ACCESS = 'data_products_platform_access',
+    CREATE_DATA_PRODUCT_STARTED = 'create_data_product_started',
+    CREATE_DATA_PRODUCT_COMPLETED = 'create_data_product_completed',
 }
