933d1d280476b7e853350fa5115937971c333a34
Lowie Debois <lowie.debois@dataminded.com>
2025-07-09 15:14:22 +0200

    added POSTHOG_ENABLED environment variable

--- frontend/src/config/posthog-config.ts
+++ frontend/src/config/posthog-config.ts
@@ -7,4 +7,11 @@ posthog.init(config.POSTHOG_KEY, {
     person_profiles: 'never', // do not link events with personal information (profiles)
 });
 
+if (config.POSTHOG_ENABLED) {
+    posthog.opt_in_capturing(); // technically redundant (is default)
+} else {
+    posthog.opt_out_capturing();
+}
+    
+
 export default posthog;
