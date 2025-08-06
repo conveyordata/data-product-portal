71944d01a6f793caf26eae2243055743fe31739c
Lowie Debois <lowie.debois@gmail.com>
2025-08-06 15:43:41 +0200

    added straight edge margin

--- frontend/src/components/charts/custom-edges/straight-edge.tsx
+++ frontend/src/components/charts/custom-edges/straight-edge.tsx
@@ -1,18 +1,35 @@
 import { BaseEdge, type EdgeProps, getStraightPath } from '@xyflow/react';
 
 export function StraightEdge({ id, sourceX, sourceY, targetX, targetY, style }: EdgeProps) {
-    const [edgePath] = getStraightPath({
-        sourceX,
-        sourceY,
-        targetX,
-        targetY,
-    });
+    // Define a margin to stop the edge before the node boundary (hard coded... based on size of node would be better code practice)
+    const margin = 35;
+    
+    // angle
+    const angle = Math.atan2(targetY - sourceY, targetX - sourceX);
+    const angleDeg = angle * (180 / Math.PI);
 
+    // Adjust coordinates based on the margin
+    const cos = margin * Math.cos(angle);
+    const sin = margin * Math.sin(angle);
+
+    const adjustedSourceX = sourceX + cos;
+    const adjustedSourceY = sourceY + sin;
+    
+    const adjustedTargetX = targetX - cos;
+    const adjustedTargetY = targetY - sin;
+    
+    // Midpoint for arrow
     const midX = (sourceX + targetX) / 2
     const midY = (sourceY + targetY) / 2
+    
 
-    // angle for arrow rotation
-    const angle = Math.atan2(targetY - sourceY, targetX - sourceX) * (180 / Math.PI)
+    const [edgePath] = getStraightPath({
+        sourceX: adjustedSourceX,
+        sourceY: adjustedSourceY,
+        targetX: adjustedTargetX,
+        targetY: adjustedTargetY,
+    });
+
 
     return (
         <>
@@ -23,7 +40,7 @@ export function StraightEdge({ id, sourceX, sourceY, targetX, targetY, style }: 
                 fill="currentColor"
                 stroke="currentColor"
                 strokeWidth="1"
-                transform={`translate(${midX}, ${midY}) rotate(${angle}) translate(-6, -4)`}
+                transform={`translate(${midX}, ${midY}) rotate(${angleDeg}) translate(-6, -4)`}
                 style={{
                 fill: style?.stroke || "#b1b1b7",
                 stroke: style?.stroke || "#b1b1b7",
