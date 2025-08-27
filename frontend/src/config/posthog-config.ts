b687a7bdb4502d0fd806a31caf39201929556543
Stijn Janssens <janssens.stijn@hotmail.com>
2025-08-27 10:51:56 +0200

    Correct disabling of posthog analytics

--- frontend/src/config/posthog-config.ts
+++ frontend/src/config/posthog-config.ts
@@ -1,14 +1,13 @@
 import posthog from 'posthog-js';
 
-posthog.init(config.POSTHOG_KEY, {
-    api_host: config.POSTHOG_HOST,
-    autocapture: false,
-    capture_pageview: false,
-    disable_session_recording: true,
-    person_profiles: 'identified_only',
-});
-
 if (config.POSTHOG_ENABLED) {
+    posthog.init(config.POSTHOG_KEY, {
+        api_host: config.POSTHOG_HOST,
+        autocapture: false,
+        capture_pageview: false,
+        disable_session_recording: true,
+        person_profiles: 'identified_only',
+    });
     posthog.opt_in_capturing(); // technically redundant (is default)
 } else {
     posthog.opt_out_capturing();
