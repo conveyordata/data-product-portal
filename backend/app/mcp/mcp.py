010fb531da0a920402224e87a5ea690c12ae49df
Stijn Janssens <janssens.stijn@hotmail.com>
2025-09-08 11:08:04 +0200

    Old BearerAuthProvider

--- backend/app/mcp/mcp.py
+++ backend/app/mcp/mcp.py
@@ -2,7 +2,9 @@ from typing import Any, Dict, Optional
 from uuid import UUID
 
 from fastmcp import Context, FastMCP
-from fastmcp.server.auth.providers.jwt import JWTVerifier
+
+# from fastmcp.server.auth.providers.jwt import JWTVerifier
+from fastmcp.server.auth import BearerAuthProvider
 from fastmcp.server.dependencies import AccessToken, get_access_token
 from sqlalchemy.orm import configure_mappers
 
@@ -57,11 +59,13 @@ def initialize_models():
 initialize_models()  # TODO Figure out if this is still needed
 
 
-def get_auth_provider() -> Optional[JWTVerifier]:
+def get_auth_provider() -> Optional[BearerAuthProvider]:
     if settings.OIDC_ENABLED:
         print(get_oidc().authority)
         print(get_oidc().jwks_uri)
-        return JWTVerifier(issuer=get_oidc().authority, jwks_uri=get_oidc().jwks_uri)
+        return BearerAuthProvider(
+            issuer=get_oidc().authority, jwks_uri=get_oidc().jwks_uri
+        )
     return None
 
 
