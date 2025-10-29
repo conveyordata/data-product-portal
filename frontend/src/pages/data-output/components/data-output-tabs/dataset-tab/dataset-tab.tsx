12397171ad81b7c1a5a64f85308d8dcd994da1a5
Stijn Janssens <janssens.stijn@hotmail.com>
2025-10-29 09:53:44 +0100

    Remove button to link technical asset to output port from the tech asset detail screen (#1919)

--- frontend/src/pages/data-output/components/data-output-tabs/dataset-tab/dataset-tab.tsx
+++ frontend/src/pages/data-output/components/data-output-tabs/dataset-tab/dataset-tab.tsx
@@ -1,17 +1,13 @@
-import { Button, Flex, Form } from 'antd';
+import { Flex, Form } from 'antd';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Searchbar } from '@/components/form';
-import { useModal } from '@/hooks/use-modal.tsx';
-import { useCheckAccessQuery } from '@/store/features/authorization/authorization-api-slice.ts';
 import { useGetDataOutputByIdQuery } from '@/store/features/data-outputs/data-outputs-api-slice.ts';
 import { useGetDataProductByIdQuery } from '@/store/features/data-products/data-products-api-slice.ts';
-import { AuthorizationAction } from '@/types/authorization/rbac-actions.ts';
 import type { DataOutputDatasetLink } from '@/types/data-output';
 import type { SearchForm } from '@/types/shared';
 
-import { AddDatasetPopup } from '../../../../data-product/components/data-product-tabs/data-output-tab/components/add-dataset-popup/add-dataset-popup.tsx';
 import { DatasetTable } from './components/dataset-table/dataset-table.component.tsx';
 import styles from './dataset-tab.module.scss';
 
@@ -30,7 +26,6 @@ type Props = {
 };
 export function DatasetTab({ dataOutputId }: Props) {
     const { t } = useTranslation();
-    const { isVisible, handleOpen, handleClose } = useModal();
 
     const { data: dataOutput, isFetching: isFetchingInitialValues } = useGetDataOutputByIdQuery(dataOutputId || '', {
         skip: !dataOutputId,
@@ -45,37 +40,15 @@ export function DatasetTab({ dataOutputId }: Props) {
         return filterDatasets(dataOutput?.dataset_links ?? [], searchTerm);
     }, [dataOutput?.dataset_links, searchTerm]);
 
-    const { data: access } = useCheckAccessQuery(
-        {
-            resource: dataProduct?.id,
-            action: AuthorizationAction.DATA_PRODUCT__REQUEST_DATA_OUTPUT_LINK,
-        },
-        { skip: !dataProduct },
-    );
-    const canAddDataset = access?.allowed || false;
-
     return (
-        <>
-            <Flex vertical className={`${styles.container} ${filteredDatasets.length === 0 && styles.paginationGap}`}>
-                <Searchbar
-                    placeholder={t('Search output ports by name')}
-                    formItemProps={{ initialValue: '', className: styles.marginBottomLarge }}
-                    form={searchForm}
-                    actionButton={
-                        <Button
-                            disabled={!canAddDataset}
-                            type={'primary'}
-                            className={styles.formButton}
-                            onClick={handleOpen}
-                        >
-                            {t('Add Output port')}
-                        </Button>
-                    }
-                />
+        <Flex vertical className={`${styles.container} ${filteredDatasets.length === 0 && styles.paginationGap}`}>
+            <Searchbar
+                placeholder={t('Search output ports by name')}
+                formItemProps={{ initialValue: '', className: styles.marginBottomLarge }}
+                form={searchForm}
+            />
 
-                <DatasetTable dataProductId={dataProduct?.id} dataOutputId={dataOutputId} datasets={filteredDatasets} />
-            </Flex>
-            {isVisible && <AddDatasetPopup onClose={handleClose} isOpen={isVisible} dataOutputId={dataOutputId} />}
-        </>
+            <DatasetTable dataProductId={dataProduct?.id} dataOutputId={dataOutputId} datasets={filteredDatasets} />
+        </Flex>
     );
 }
