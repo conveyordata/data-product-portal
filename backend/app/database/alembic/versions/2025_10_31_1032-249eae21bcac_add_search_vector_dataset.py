0578b7b88467f34c169a4ecf9a89e7e3b1cba073
nielsc <niels.claeys@gmail.com>
2025-11-02 23:15:08 +0100

    Fix alembic

--- backend/app/database/alembic/versions/2025_10_31_1032-249eae21bcac_add_search_vector_dataset.py
+++ backend/app/database/alembic/versions/2025_10_31_1032-249eae21bcac_add_search_vector_dataset.py
@@ -14,7 +14,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "249eae21bcac"
-down_revision: Union[str, None] = "d56d796fb20d"
+down_revision: Union[str, None] = "f0b94d78c72c"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
