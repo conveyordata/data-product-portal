ef4a6fe80deaaface3ef4ab8fb7ce27b0c6f5cce
Stijn Janssens <janssens.stijn@hotmail.com>
2025-11-05 14:03:45 +0100

    Feature/rework pending request component (#1983)
    * Use Item.Meta for pending requests
    
    * Cleanup creation of pending request code
    
    * Move handleAccept and handleDeny inside the item type
    
    * Move handleAccept and handleDeny inside the item type
    
    * Merge pendingActions and slicedPendingActions into one function
    
    * Solved the pending requests a little bit more by extracting into separate components-n
    
    * Cleanup file even more
    
    * Fixed comments

--- frontend/src/pages/home/components/pending-requests-inbox/pending-requests-list.tsx
+++ frontend/src/pages/home/components/pending-requests-inbox/pending-requests-list.tsx
@@ -1,118 +1,34 @@
-import { UserOutlined } from '@ant-design/icons';
-import { Avatar, Button, Flex, List, Typography } from 'antd';
+import { List } from 'antd';
 import type { PaginationConfig } from 'antd/es/pagination';
-import type { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router';
 import { EmptyList } from '@/components/empty/empty-list/empty-list.component';
 import styles from '@/pages/home/components/pending-requests-inbox/pending-requests-inbox.module.scss';
-import type { ActionResolveRequest, PendingActionTypes } from '@/types/pending-actions/pending-actions';
-import { formatDate } from '@/utils/date.helper.ts';
-
-export type PendingActionItem = {
-    key: string;
-    description: ReactNode;
-    navigatePath: string;
-    date: string;
-    author: string;
-    initials: string;
-    message: ReactNode;
-    color: string;
-    tag: ReactNode;
-    request: ActionResolveRequest;
-    icon: ReactNode;
-    type: PendingActionTypes;
-};
+import type { PendingAction } from '@/types/pending-actions/pending-actions';
+import { PendingItem } from './pending-request-item';
 
 type PendingRequestsListProps = {
-    pendingActionItems: PendingActionItem[];
+    pendingActions: PendingAction[];
     pagination: PaginationConfig;
-    onAccept: (request: ActionResolveRequest) => void;
-    onReject: (request: ActionResolveRequest) => void;
 };
-export const PendingRequestsList = ({
-    pendingActionItems,
-    pagination,
-    onAccept,
-    onReject,
-}: PendingRequestsListProps) => {
+
+export const PendingRequestsList = ({ pendingActions, pagination }: PendingRequestsListProps) => {
     const { t } = useTranslation();
 
-    const navigate = useNavigate();
-
-    if (!pendingActionItems || pendingActionItems.length === 0) {
+    if (!pendingActions || pendingActions.length === 0) {
         return <EmptyList description={t('No requests available.')} />;
     }
 
-    const handleItemClick = (navigatePath: string) => {
-        navigate(navigatePath);
-    };
-
     return (
         <List
-            dataSource={pendingActionItems}
+            dataSource={pendingActions}
             pagination={{
                 ...pagination,
                 className: styles.antListPagination,
             }}
+            itemLayout="vertical"
+            size="large"
             split={false}
-            renderItem={(item) => {
-                const formattedDate = item.date ? formatDate(item.date) : '';
-                return (
-                    <List.Item
-                        key={item.key}
-                        className={styles.listItem}
-                        onClick={() => handleItemClick(item.navigatePath)}
-                        actions={[
-                            <Button
-                                key={'accept'}
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    onAccept(item.request);
-                                }}
-                                type="link"
-                                size={'small'}
-                            >
-                                {t('Accept')}
-                            </Button>,
-                            <Button
-                                key={'reject'}
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    onReject(item.request);
-                                }}
-                                type="link"
-                                size={'small'}
-                            >
-                                {t('Reject')}
-                            </Button>,
-                        ]}
-                    >
-                        <Flex vertical className={styles.itemCard} gap={'small'}>
-                            <Flex align="center">
-                                <Avatar style={{ backgroundColor: item.color }} className={styles.avatar} size="large">
-                                    {item.initials || <UserOutlined />}
-                                </Avatar>
-                                <Flex vertical flex={1}>
-                                    <Flex justify="space-between">
-                                        <Typography.Text>{item.description}</Typography.Text>
-                                        <Flex>
-                                            <Flex gap="small">
-                                                <Typography.Text type="secondary">{item.icon}</Typography.Text>
-                                                <Typography.Text>{item.tag}</Typography.Text>
-                                            </Flex>
-                                        </Flex>
-                                    </Flex>
-                                    <Typography.Text type="secondary">
-                                        by {item.author}, {formattedDate}
-                                    </Typography.Text>
-                                </Flex>
-                            </Flex>
-                            <Typography.Text>{item.message}</Typography.Text>
-                        </Flex>
-                    </List.Item>
-                );
-            }}
+            renderItem={(item) => <PendingItem pendingAction={item} />}
         />
     );
 };
