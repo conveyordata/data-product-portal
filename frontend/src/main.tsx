7b06f102662a4eb2eb9669e228e0760179542504
Stijn De Haes <stijndehaes@gmail.com>
2025-11-07 15:00:18 +0100

    Fix bug with unexisting dataproduct id
    When a data product does not exist,
    or the user does not have access to it anymore
    we don't use it for cart input.
    
    In support of: #1917

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import './i18n.ts';
 import '@ant-design/v5-patch-for-react-19';
 
 import { PostHogProvider } from 'posthog-js/react';
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import { AuthProvider } from 'react-oidc-context';
@@ -29,11 +29,9 @@ ReactDOM.createRoot(root).render(
             <AuthProvider {...oidcAuthProviderProps}>
                 <Provider store={store}>
                     <I18nextProvider i18n={i18n}>
-                        <Suspense fallback={''}>
-                            <MessageListener />
-                            <NotificationListener />
-                            <App />
-                        </Suspense>
+                        <MessageListener />
+                        <NotificationListener />
+                        <App />
                     </I18nextProvider>
                 </Provider>
             </AuthProvider>
