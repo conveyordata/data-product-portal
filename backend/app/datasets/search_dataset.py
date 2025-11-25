5a4965b73b17e1762956dccb61ef99ec59c4850b
Stijn Janssens <janssens.stijn@hotmail.com>
2025-11-25 15:38:33 +0100

    Fix sql injection error

--- backend/app/datasets/search_dataset.py
+++ backend/app/datasets/search_dataset.py
@@ -10,8 +10,10 @@ def recalculate_search_vector_statement(dataset_id: UUID | None = None) -> TextC
         "Recalculating search vector for"
         + f"{str(dataset_id) if dataset_id else 'all datasets'}"
     )
-    return text(
-        """
+
+    if dataset_id:
+        return text(
+            """
 WITH data_outputs_vectors AS (
     SELECT dod.dataset_id,
         (
@@ -24,9 +26,7 @@ WITH data_outputs_vectors AS (
         ) AS search_vector
     FROM data_outputs
              JOIN data_outputs_datasets dod on dod.data_output_id = data_outputs.id
-"""
-        + ("WHERE dod.dataset_id = :dataset_id" if dataset_id else "")
-        + """
+WHERE dod.dataset_id = :dataset_id
     GROUP BY dod.dataset_id),
 dataset_search_vectors AS (
     SELECT datasets.id,
@@ -41,14 +41,46 @@ dataset_search_vectors AS (
         ) AS new_search_vector
     FROM datasets
     LEFT JOIN data_outputs_vectors dov ON dov.dataset_id = datasets.id
-    """
-        + ("WHERE datasets.id = :dataset_id" if dataset_id else "")
-        + """)
+    WHERE datasets.id = :dataset_id)
 UPDATE datasets ds
 SET search_vector = csv.new_search_vector FROM dataset_search_vectors csv
 WHERE ds.id = csv.id;
 """
-    )
+        ).bindparams(dataset_id=dataset_id)
+    else:
+        return text(
+            """
+WITH data_outputs_vectors AS (
+    SELECT dod.dataset_id,
+        (
+            setweight(to_tsvector('english',
+                string_agg(coalesce(data_outputs.name, ''), ' ')),
+                      'A') ||
+            setweight(to_tsvector('english',
+                string_agg(coalesce(data_outputs.description, ''), ' ')),
+                      'B')
+        ) AS search_vector
+    FROM data_outputs
+             JOIN data_outputs_datasets dod on dod.data_output_id = data_outputs.id
+    GROUP BY dod.dataset_id),
+dataset_search_vectors AS (
+    SELECT datasets.id,
+        (
+            setweight(
+                    to_tsvector('english', coalesce(datasets.name, '')),
+                    'A') ||
+            setweight(
+                    to_tsvector('english', coalesce(datasets.description, '')),
+                    'B') ||
+            coalesce(dov.search_vector, to_tsvector('english', ''))
+        ) AS new_search_vector
+    FROM datasets
+    LEFT JOIN data_outputs_vectors dov ON dov.dataset_id = datasets.id)
+UPDATE datasets ds
+SET search_vector = csv.new_search_vector FROM dataset_search_vectors csv
+WHERE ds.id = csv.id;
+"""
+        )
 
 
 def recalculate_search_vector_dataset_statement(dataset_id: UUID) -> TextClause:
