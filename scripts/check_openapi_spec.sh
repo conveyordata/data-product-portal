0190f465d0b0ec8782401e0a49be6a6187974b3c
Stijn De Haes <stijndehaes@gmail.com>
2025-11-26 17:10:11 +0100

    Add regen openapi spec to pre-commit
    Also changed the way it works, it only
    checks if that single file is dirty
    now

--- scripts/check_openapi_spec.sh
+++ scripts/check_openapi_spec.sh
@@ -7,4 +7,15 @@ pushd backend
   poetry run python -m app.open_api_export ../docs/static/openapi.json
 popd
 
-./scripts/check_git_dirty.sh
+#!/usr/bin/env bash
+set -e
+
+if [[ -z "$(git status --porcelain docs/static/openapi.json)" ]];
+then
+  exit 0
+else
+  echo "Git is dirty"
+  git status --porcelain docs/static/openapi.json
+  git --no-pager diff
+  exit 1
+fi
