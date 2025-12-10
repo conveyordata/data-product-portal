f85ac74d4aa4b00669e8b06044bf9a4b52b38487
Stijn De Haes <stijn.de.haes@gmail.com>
2025-12-10 16:39:33 +0100

    Only run git check when running in CI (#2261)

--- scripts/check_openapi_spec.sh
+++ scripts/check_openapi_spec.sh
@@ -9,15 +9,14 @@ pushd backend
   poetry run python -m app.open_api_export ../docs/static/openapi.json
 popd
 
-#!/usr/bin/env bash
-set -e
-
-if [[ -z "$(git status --porcelain docs/static/openapi.json)" ]];
-then
-  exit 0
-else
-  echo "Git is dirty"
-  git status --porcelain docs/static/openapi.json
-  git --no-pager diff
-  exit 1
+if [[ -n "${CI}" ]]; then
+  if [[ -z "$(git status --porcelain docs/static/openapi.json)" ]];
+  then
+    exit 0
+  else
+    echo "Git is dirty"
+    git status --porcelain docs/static/openapi.json
+    git --no-pager diff
+    exit 1
+  fi
 fi
