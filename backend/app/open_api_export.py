2d60ce973d18ca765c0756529ae5b44f38aa31eb
Robbert Hofman <robbert.hofman@dataminded.com>
2025-12-29 16:51:52 +0100

    various changes to just be able to commit

--- backend/app/open_api_export.py
+++ backend/app/open_api_export.py
@@ -4,8 +4,9 @@ import json
 from pathlib import Path
 
 import typer
+from fastapi.openapi.utils import get_openapi
 
-from app.main import app as f_app
+from app.main import app as fastapi_app
 
 app = typer.Typer(
     help="Generate the openapi spec of the backend and output as a json file./."
@@ -15,11 +16,27 @@ app = typer.Typer(
 @app.command()
 def export_openapi(
     output: Path = typer.Argument(default="openapi.json", help="Path where to export"),
+    openapi_version: str | None = typer.Option(
+        default=None,
+        help="If set, overrides the openapi version in the output. This is useful for compatibility with tools that do not support OpenAPI 3.1.",
+    ),
 ):
+    # generate dict
+    result = get_openapi(
+        title=fastapi_app.title,
+        version=fastapi_app.version,
+        contact=fastapi_app.contact,
+        routes=fastapi_app.routes,
+        summary=fastapi_app.summary,
+        description=fastapi_app.description,
+        openapi_version=openapi_version or "3.1.0",
+    )
+
+    # write to file
     output = Path(output)
     output.parent.mkdir(parents=True, exist_ok=True)
     output.write_text(
-        json.dumps(f_app.openapi(), indent=2) + "\n",
+        json.dumps(result, indent=2) + "\n",
     )
 
 
