feaa2613c91e9645d47d1047982e5217d57d2b58
Robbert Hofman <robbert.hofman@dataminded.com>
2026-01-31 23:45:53 +0100

    generated sdk takes place of old sdk

--- sdk-go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/data-product-portal/sdk-go
-
-go 1.21
--- sdk-go/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
-	Name  string `json:"name,omitempty"`
-}
