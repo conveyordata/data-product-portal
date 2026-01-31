8b596c13dece7d89591765402f100b27df7a7a1f
Robbert Hofman <robbert.hofman@dataminded.com>
2026-01-31 22:10:13 +0100

    add route filter to exclude deprecated routes

--- backend/app/open_api_export.py
+++ backend/app/open_api_export.py
@@ -5,7 +5,9 @@ from pathlib import Path
 
 import typer
 from fastapi.openapi.utils import get_openapi
+from fastapi.routing import APIRoute, BaseRoute
 
+from app.core.logging import logger
 from app.main import app as fastapi_app
 
 app = typer.Typer(
@@ -13,23 +15,40 @@ app = typer.Typer(
 )
 
 
+def _route_filter(routes: list[BaseRoute]) -> list[BaseRoute]:
+    """
+    returns a selection of routes that we want to use for SDK generation
+    """
+    result = []
+    for route in routes:
+        if isinstance(route, APIRoute) and route.path.startswith("/api/v2"):
+            if not getattr(route, "deprecated"):
+                result.append(route)
+            else:
+                logger.debug(f"Excluding v2 deprecated route: {route.path}")
+    return result
+
+
 @app.command()
 def export_openapi(
     output: Path = typer.Argument(default="openapi.json", help="Path where to export"),
-    openapi_version: str | None = typer.Option(
-        default=None,
-        help="If set, overrides the openapi version in the output. This is useful for compatibility with tools that do not support OpenAPI 3.1.",
+    exclude_deprecated: bool = typer.Option(
+        default=False,
+        help="When set to true, only includes non-deprecated v2 API routes",
     ),
 ):
+    routes = fastapi_app.routes
+    if exclude_deprecated:
+        routes = _route_filter(routes)
+
     # generate dict
     result = get_openapi(
         title=fastapi_app.title,
         version=fastapi_app.version,
         contact=fastapi_app.contact,
-        routes=fastapi_app.routes,
-        summary=fastapi_app.summary,
+        routes=routes,
+        summary=fastapi_app.summary if not exclude_deprecated else None,
         description=fastapi_app.description,
-        openapi_version=openapi_version or "3.1.0",
     )
 
     # write to file
