6edd540cab006df74a2353f18ebca494105c8621
Robbert Hofman <robbert.hofman@dataminded.com>
2026-02-01 01:21:23 +0100

    terraform-plugin-docs

---
+++ terraform-provider-dataproductportal/examples/data-sources/dataproductportal_data_product/data-source.tf
@@ -0,0 +1,3 @@
+data "dataproductportal_data_product" "example" {
+  id = "00000000-0000-0000-0000-000000000001"
+}
---
+++ terraform-provider-dataproductportal/examples/provider/provider.tf
@@ -0,0 +1,4 @@
+provider "dataproductportal" {
+  base_url = "https://portal.example.com/api"
+  api_key  = var.api_key
+}
