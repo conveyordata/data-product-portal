1aa8ab3c65ba2930b4b835262a18cfb7e3f10408
vlieven <lieven.verswyvel@gmail.com>
2026-02-05 21:03:18 +0100

    Regen openapi.json

--- frontend/src/store/api/services/generated/configurationDataProductSettingsApi.ts
+++ frontend/src/store/api/services/generated/configurationDataProductSettingsApi.ts
@@ -121,8 +121,6 @@ export type ValidationError = {
   loc: (string | number)[];
   msg: string;
   type: string;
-  input?: any;
-  ctx?: object;
 };
 export type HttpValidationError = {
   detail?: ValidationError[];
