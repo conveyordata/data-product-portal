4b63b2cd3d9cd9ca874dff27a2e7ef78d71aa5ed
vlieven <lieven.verswyvel@gmail.com>
2026-03-19 13:11:12 +0100

    Add banner for pending requests to home page

--- frontend/src/pages/home/home.page.tsx
+++ frontend/src/pages/home/home.page.tsx
@@ -2,6 +2,7 @@ import { Col, Row } from 'antd';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useBreadcrumbs } from '@/components/layout/navbar/breadcrumbs/breadcrumb.context.tsx';
+import AlertBanner from '@/pages/home/components/alert-banner/alert-banner.tsx';
 import { DataProductsInbox } from '@/pages/home/components/data-products-inbox/data-products-inbox.tsx';
 import { DatasetsInbox } from '@/pages/home/components/datasets-inbox/datasets-inbox.tsx';
 import { QuickActions } from '@/pages/home/components/quick-actions/quick-actions.tsx';
@@ -22,6 +23,8 @@ export function Home() {
 
     return (
         <div className={styles.container}>
+            <AlertBanner />
+
             <div className={styles.contentSecondary}>
                 <QuickActions style={{ marginBottom: 48 }} />
 
