34fdd287b04de1b494e08d6d0592732614cd6f8d
vlieven <lieven.verswyvel@gmail.com>
2026-03-20 16:24:29 +0100

    Fix formatting

--- frontend/src/components/layout/sidebar/sidebar.component.tsx
+++ frontend/src/components/layout/sidebar/sidebar.component.tsx
@@ -87,7 +87,7 @@ export const Sidebar = () => {
 
     return (
         <Layout.Sider className={styles.sidebarWrapper}>
-            <Flex vertical justify="space-between" style={{height: "100%"}}>
+            <Flex vertical justify="space-between" style={{ height: '100%' }}>
                 <div>
                     <Flex className={styles.logoContainer}>
                         <ProductLogo
@@ -107,7 +107,7 @@ export const Sidebar = () => {
 
                 {showWizard && (
                     <Flex justify="center" style={{ width: '100%', paddingBottom: '16px' }}>
-                            <AnimatedWizard size={200} />
+                        <AnimatedWizard size={200} />
                     </Flex>
                 )}
             </Flex>
