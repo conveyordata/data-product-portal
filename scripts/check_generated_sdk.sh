291431c0b208070e50f5df05efa6347a61b0bdb7
Stijn Janssens <janssens.stijn@hotmail.com>
2026-04-16 10:59:58 +0200

    Update scripts/check_generated_sdk.sh
    Co-authored-by: Stijn De Haes <stijn.de.haes@gmail.com>

--- scripts/check_generated_sdk.sh
+++ scripts/check_generated_sdk.sh
@@ -2,30 +2,33 @@
 set -e
 
 # Check if the generated API is up to date
-cd sdk
 
 echo "Checking if generated SDK is up to date..."
+pushd sdk
 
-# Install dependencies (including openapi-python-client)
-poetry install --no-interaction --only dev
 
-# Run generate-api
-poetry run openapi-python-client generate \
-  --path ./../docs/static/openapi.json \
-  --output-path ./sdk/api_client \
-  --meta none \
-  --overwrite
+  # Install dependencies (including openapi-python-client)
+  poetry install --no-interaction --only dev
+  
+  # Run generate-api
+  poetry run openapi-python-client generate \
+    --path ./../docs/static/openapi.json \
+    --output-path ./sdk/api_client \
+    --meta none \
+    --overwrite
 
-if [[ -n "${CI}" ]]; then
-  if [[ -z "$(git status --porcelain .)" ]];
-  then
-    exit 0
-  else
-    echo "Git is dirty"
-    git status --porcelain .
-    git --no-pager diff
-    exit 1
+
+  if [[ -n "${CI}" ]]; then
+    if [[ -z "$(git status --porcelain .)" ]];
+    then
+      exit 0
+    else
+      echo "Git is dirty"
+      git status --porcelain .
+      git --no-pager diff
+      exit 1
+    fi
   fi
-fi
 
+popd
 echo "✅ Generated API files are up to date."
