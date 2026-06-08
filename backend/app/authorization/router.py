e2b148d94059408c7a223a66da446e5d0bb95a30
Stijn Janssens <janssens.stijn@hotmail.com>
2026-06-08 15:09:33 +0200

    Revert no deprecated openapi spec stuff

--- backend/app/authorization/router.py
+++ backend/app/authorization/router.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends
 
 from app.authorization.schema_response import AccessResponse, IsAdminResponse
 from app.core.auth.auth import get_authenticated_user
@@ -29,8 +29,8 @@ router = APIRouter(tags=["Authorization"], prefix="/v2/authz")
 )
 def check_access(
     action: Action,
-    resource: Optional[UUID] = Query(None),
-    domain: Optional[UUID] = Query(None),
+    resource: Optional[UUID] = None,
+    domain: Optional[UUID] = None,
     user: User = Depends(get_authenticated_user),
 ) -> AccessResponse:
     """Allows the requesting user to check whether an access check will fail or succeed.
