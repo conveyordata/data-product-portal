42ff64c96af6bcfd0efdf9c74489f1f34ca902c8
vlieven <lieven.verswyvel@gmail.com>
2026-07-02 16:42:37 +0200

    Allow deleting an Exploration from the frontend

--- frontend/src/utils/routes.helper.ts
+++ frontend/src/utils/routes.helper.ts
@@ -1,43 +1,5 @@
-import { ApplicationPaths, DynamicPathParams } from '@/types/navigation.ts';
+import type { DynamicPathParams } from '@/types/navigation.ts';
 
 export function getDynamicRoutePath(path: string, param: DynamicPathParams, value: string) {
     return path.replace(`:${param}`, value);
-}
-
-export function isDataProductEditPage(path: string, dataProductId: string) {
-    return (
-        getDynamicRoutePath(ApplicationPaths.DataProductEdit, DynamicPathParams.DataProductId, dataProductId) === path
-    );
-}
-
-export function isDatasetEditPage(path: string, datasetId?: string, dataProductId?: string) {
-    if (!datasetId || !dataProductId) {
-        return false;
-    }
-    return (
-        path ===
-        ApplicationPaths.MarketPlaceOutputPortEdit.replace(`:${DynamicPathParams.DatasetId}`, datasetId).replace(
-            `:${DynamicPathParams.DataProductId}`,
-            dataProductId,
-        )
-    );
-}
-
-export function isProductStudioOutputPortEditPage(path: string, datasetId: string, dataProductId: string) {
-    return (
-        path ===
-        ApplicationPaths.OutputPortEdit.replace(`:${DynamicPathParams.DatasetId}`, datasetId).replace(
-            `:${DynamicPathParams.DataProductId}`,
-            dataProductId,
-        )
-    );
-}
-
-export function isDataOutputEditPage(path: string, dataOutputId: string, dataProductId: string) {
-    return (
-        getDynamicRoutePath(ApplicationPaths.DataOutputEdit, DynamicPathParams.DataOutputId, dataOutputId).replace(
-            `:${DynamicPathParams.DataProductId}`,
-            dataProductId,
-        ) === path
-    );
 }
