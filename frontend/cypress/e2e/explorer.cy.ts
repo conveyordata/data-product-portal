015f8d5f11097be53b56277298b71b849f3ab29e
Stijn Janssens <janssens.stijn@hotmail.com>
2026-09-16 10:28:34 +0200

    Work with data-cy for graph checks

--- frontend/cypress/e2e/explorer.cy.ts
+++ frontend/cypress/e2e/explorer.cy.ts
@@ -5,7 +5,7 @@ describe('Explorer page', () => {
         cy.contains('h1, h2, h3', 'Explorer').should('be.visible');
 
         // The graph is rendered by @xyflow/react once the graph data query resolves.
-        cy.get('.react-flow__node', { timeout: 20000 }).should('have.length.greaterThan', 0);
-        cy.get('.react-flow__edge').should('have.length.greaterThan', 0);
+        cy.get('[data-cy="graph-node"]', { timeout: 20000 }).should('have.length.greaterThan', 0);
+        cy.get('[data-cy="graph-edge"]').should('have.length.greaterThan', 0);
     });
 });
