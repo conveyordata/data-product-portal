4b59f3ca6cb37d73dd18fcad5649d48da14c8375
Stijn De Haes <stijndehaes@gmail.com>
2026-09-22 16:47:02 +0200

    ALLIGN LOOKS WEIRD

--- frontend/src/pages/output-port/output-port.page.tsx
+++ frontend/src/pages/output-port/output-port.page.tsx
@@ -96,8 +96,10 @@ export function OutputPort() {
                 <Flex className={styles.datasetHeaderContainer}>
                     <Flex className={styles.datasetHeader} align="center">
                         <CustomSvgIconLoader iconComponent={outputPortBorderIcon} size="large" />
-                        <Typography.Title level={3}>{outputPort?.name}</Typography.Title>
-                        <OutputPortAccessIcon accessType={outputPort.access_type} />
+                        <Flex align="baseline" gap="small">
+                            <Typography.Title level={3}>{outputPort?.name}</Typography.Title>
+                            <OutputPortAccessIcon accessType={outputPort.access_type} />
+                        </Flex>
                     </Flex>
                     {canEdit && (
                         <CircleIconButton
